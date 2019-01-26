896673d48e003cd4a9d8ab2e2cf82c52bf6f2dc0
David Omar Flores Chávez <davidomarfch@gmail.com>
2019-01-25 23:28:26 -0600

    Stop adding external-site icon to archive items
      - Add class "no-target-blank" to items to avoid
      - Add condition for not having that class in script

--- assets/js/targetBlank.js
+++ assets/js/targetBlank.js
@@ -1,7 +1,7 @@
 let aHrefElements = document.getElementsByTagName("a");
 
 for (let i = 0; i < aHrefElements.length; i++){
-    if (aHrefElements[i].target == "_blank"){
+    if (aHrefElements[i].target == "_blank" && !aHrefElements[i].classList.contains("no-target-blank")){
         aHrefElements[i].innerHTML += ' <i class="fas fa-external-link-alt fa-xs"></i>';
     }
 }
